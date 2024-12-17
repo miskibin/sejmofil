3b141154ad737b7843a4708e87942fa51ced1b01
miskibin <michal.skibinski@nokia.com>
2024-12-17 11:37:20 +0100

    Refactor ProcessSearchPage to use useEffect for data fetching and manage state with useState for improved performance and organization

--- app/process/page.tsx
+++ app/process/page.tsx
@@ -1,23 +1,37 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { getAllTopics, getPrintsRelatedToTopic } from "@/lib/queries";
 import ProcessSearchClient from "./processSearch";
+import { Print, Topic } from "@/lib/types";
 
-export default async function ProcessSearchPage() {
-  const topics = await getAllTopics();
+export default function ProcessSearchPage() {
+  const [topics, setTopics] = useState<Topic[]>([]);
+  const [flatPrints, setFlatPrints] = useState<Print[]>([]);
+  const [topicsMap, setTopicsMap] = useState<Record<string, string[]>>({});
 
-  const topicsMap: Record<string, string[]> = {};
-  const printsPromises = topics.map(async (topic) => {
-    const prints = await getPrintsRelatedToTopic(topic.name);
-    prints.forEach((print) => {
-      if (!topicsMap[print.number]) {
-        topicsMap[print.number] = [];
-      }
-      topicsMap[print.number].push(topic.name);
-    });
-    return prints;
-  });
-  const allPrints = await Promise.all(printsPromises);
-  const flatPrints = allPrints.flat();
+  useEffect(() => {
+    const fetchData = async () => {
+      const topics = await getAllTopics();
+      setTopics(topics);
+
+      const topicsMap: Record<string, string[]> = {};
+      const printsPromises = topics.map(async (topic) => {
+        const prints = await getPrintsRelatedToTopic(topic.name);
+        prints.forEach((print) => {
+          if (!topicsMap[print.number]) {
+            topicsMap[print.number] = [];
+          }
+          topicsMap[print.number].push(topic.name);
+        });
+        return prints;
+      });
+      const allPrints = await Promise.all(printsPromises);
+      const flatPrints = allPrints.flat();
+      setFlatPrints(flatPrints);
+      setTopicsMap(topicsMap);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="container mx-auto p-4 max-w-2xl">
