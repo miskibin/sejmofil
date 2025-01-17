adc409fa5a4849c2439f2497480604d55ea7b098
miskibin <michal.skibinski@nokia.com>
2025-01-17 13:46:03 +0100

    fix callback url

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,6 +8,7 @@ export async function GET(request: Request) {
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
 
+  console.log(code, searchParams, origin);
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
